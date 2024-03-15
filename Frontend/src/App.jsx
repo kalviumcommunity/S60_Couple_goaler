0382e5654103c2aa15efd975617631bd01037c1b
Prasanna V <you@example.com>
2024-03-15 13:07:10 +0530

    coded

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  axios.get("http://localhost:")
   return (
     <>
       <div>
