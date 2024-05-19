89d83accec892dd35f3289eb2075462160f9df9b
Prasanna V <you@example.com>
2024-05-19 11:47:34 +0530

    added cookies and login and signup pages

--- Backend/Routes/Routes.js
+++ Backend/Routes/Routes.js
@@ -2,8 +2,31 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const Model = require('../model');
+const Joi = require('joi');
+const bcrypt = require('bcrypt');
 let router = express.Router();
 router.use(express.json());
+
+const userJoiSchema = Joi.object({
+  name: Joi.string().alphanum().min(2).max(40).required(),
+  email: Joi.string().email().required(),
+  confirmemail: Joi.string().email().required(),
+  password: Joi.string().required(),
+  confirmpassword: Joi.string()
+    .required(),
+  gender: Joi.string().required(),
+});
+
+const userModelSchema = new mongoose.Schema({
+  name: String,
+  // profile: String,
+  email: String,
+  password: String,
+  gender: String,
+});
+
+const User = mongoose.model('AuthDB', userModelSchema);
+
 router.get('/store', (req, res) => {
   Model.find({})
     .then((data) => {
@@ -43,6 +66,51 @@ router.delete('/store/:id', async (req, res) => {
     (error) => {
       console.log(error);
     };
+  }
+});
+
+router.post('/signup', async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (user) {
+    res.send({ message: 'get out' });
+  } else {
+    const { error, value } = userJoiSchema.validate(req.body);
+    if (error) {
+      return res.status(400).json({ message: error.details[0].message });
+    }
+    if (req.body.email !== req.body.confirmemail) {
+      return res.status(400).json({ message: 'Emails do not match' });
+    }
+    if (req.body.password !== req.body.confirmpassword) {
+      return res.status(400).json({ message: 'Passwords do not match' });
+    }
+    try {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(value.password, salt);
+      const newUser = await User.create({ ...value, password: hashedPassword });
+      res.send({ message: 'ok' });
+    } catch (error) {
+      console.log('error', error);
+    }
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    const salt = await bcrypt.genSalt(10);
+
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    if (user && bcrypt.compare(req.body.password, user.password)) {
+      res.json(user);
+    } else {
+      res.send({ message: 'Invalid credentials or user not existed' });
+    }
+  } catch (error) {
+    console.log(error);
+    res.send('Error in login');
   }
 });
 
---
+++ Frontend/src/Components/Login.jsx
@@ -0,0 +1,134 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+
+function Login(props) {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      console.log(formData);
+      const res = await axios.post('http://localhost:8080/login', formData);
+      document.cookie = 'username=' + res.data.name;
+      console.log(res.data);
+      //   setFormData(res.data);
+      setIsLoggedIn(true);
+      //   console.log(res.data.message);
+    } catch (error) {
+      console.log('error on fetching', error);
+    }
+  };
+
+  const handleLogout = async () => {
+    try {
+      // Send a POST request to the /logout endpoint
+      console.log('ewj');
+      document.cookie ='username=; expires=';
+      location.reload()
+      //   setFormData({ email: '', password: '' });
+      setIsLoggedIn(false);
+    } catch (error) {
+      console.error('Error during logout:', error);
+    }
+  };
+
+  return (
+    <div className="bg-[#ffffff] h-screen ">
+      <form
+        className=" flex justify-around items-center"
+        onSubmit={handleSubmit}
+      >
+        <div className="relative py-3 w-1/3">
+          <div className="relative px-4 py-10 mx-8 md:mx-0 shadow rounded-3xl sm:p-10 border ">
+            <div className="flex items-center justify-center">
+              <h1 className="font-bold text-sky-500 text-4xl">Couple-Goaler</h1>
+            </div>
+            <br />
+            <h2 className="text-2xl font-bold text-sky-500 ">Login</h2>
+            <div className="max-w-md mx-auto text-white">
+              <div className="flex items-center space-x-5 justify-center"></div>
+              <div className="mt-5">
+                <label
+                  className="font-semibold text-sm text-gray-600 pb-1 block"
+                  htmlFor="login"
+                >
+                  E-mail
+                </label>
+                <input
+                  className="border border-slate-500 outline-none bg-gray-100 rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full  text-black"
+                  type="text"
+                  id="login"
+                  name="email"
+                  onChange={(e) => {
+                    handleChange(e);
+                  }}
+                />
+                <label
+                  className="font-semibold text-sm text-gray-600 pb-1 block"
+                  htmlFor="password"
+                >
+                  Password
+                </label>
+                <input
+                  className="border border-slate-500 outline-none bg-gray-100 rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full text-black"
+                  type="password"
+                  id="password"
+                  name="password"
+                  onChange={(e) => {
+                    handleChange(e);
+                  }}
+                />
+              </div>
+
+              <div className="mt-5">
+                <button
+                  className="py-2 px-4 bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
+                  type="submit"
+                  onClick={() => {
+                    handleSubmit;
+                  }}
+                >
+                  Log in
+                </button>
+              </div>
+
+              <div className="flex items-center justify-between mt-4">
+                <span className="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
+                <p
+                  className="text-xs text-gray-500 uppercase dark:text-gray-400 hover:underline"
+                  onClick={() => {
+                    navigate(`/Signup`);
+                  }}
+                >
+                  or sign up
+                </p>
+                <span className="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+      <div className="mt-5">
+        <button
+          className="py-2 px-4 bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
+          onClick={handleLogout}
+        >
+          Log Out
+        </button>
+      </div>
+      <div className="border border-black"></div>
+    </div>
+  );
+}
+export default Login;
